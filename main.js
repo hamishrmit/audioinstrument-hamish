fe9a81cd72c5d01e78f2b9fceed1ab7fea506f78
hamishrmit <hamishdigitalmedia@gmail.com>
2026-08-12 14:56:42 +1000

    added dialog button and audio init
    created dialog pop up with button to close it, and added event listener on dialog close to start the audio system

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+// browser loads html > browser loads js > open the dialog > user closes dialog > audio system loads > user clicks sound button
+
+//find our dialog
+const introDialog = document.getElementById("intro-dialog");
+// show the found element in our browser console
+// console.log(introDialog);
+
+const introDialogCloseButton = document.getElementById("intro-dialog-close");
+
 // find our test button
 const testButton = document.getElementById("test-button");
 // init our synth
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth();
+
+//// Dialog
+// show dialog on page load
+introDialog.showModal();
+// close dialog when user clicks
+introDialogCloseButton.addEventListener("click", function closeIntroDialog() {
+  introDialog.close();
+});
+// we put the whole function inside the event listener instead as its only called here
+
+// whenever dialog closes, initialise the audio system
+introDialog.addEventListener("close", toneInit);
+
+//// Tone
+// run to setup our audio system
+function toneInit() {
+  synth.connect(Tone.Destination);
+}
 
 // do something when this button is clicked
 testButton.addEventListener("click", playNote);
