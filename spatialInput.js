bb7e54ff2e5d1ced06759daba2a4cfc1492202c6
hamishrmit <hamishdigitalmedia@gmail.com>
2026-08-25 21:35:38 +1000

    created basic spatial input field
    used Patrick's class example as inspiration to create a spatial input area. utilised mousemove function and added styling in style.css

--- spatialInput.js
+++ spatialInput.js
@@ -1,1 +1,14 @@
-/* reference: Patrick's XYController (https://github.com/rmit-idad-2650-wed/input-event-demos) */
+/* inspired by: Patrick's XYController (https://github.com/rmit-idad-2650-wed/input-event-demos) */
+
+const xyPad = document.getElementById("xyPad");
+const marker = document.querySelector(".xyPosMarker");
+
+xyPad.addEventListener("mousemove", function (e) {
+  const rect = xyPad.getBoundingClientRect();
+
+  const x = ((e.clientX - rect.left) / rect.width) * 100;
+  const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+  marker.setAttribute("cx", x);
+  marker.setAttribute("cy", y);
+});
