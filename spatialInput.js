7838f50f067155e517f0131f48811fd7957c71c6
hamishrmit <hamishdigitalmedia@gmail.com>
2026-09-23 01:41:52 +1000

    added y-axis attack control + labels
    I made the y-axis of the spatial input control the synth envelope attack, and also added labels to each side of the spatial input to explain the sound changes

--- spatialInput.js
+++ spatialInput.js
@@ -7,14 +7,21 @@ let dragging = false;
 
 function moveMarker(e) {
   const rect = xyPad.getBoundingClientRect();
-
   const x = ((e.clientX - rect.left) / rect.width) * 100;
   const y = ((e.clientY - rect.top) / rect.height) * 100;
 
   marker.setAttribute("cx", x);
   marker.setAttribute("cy", y);
 
+  // X axis controls filter frequency
   filter.frequency.value = 200 + (x / 100) * 4800;
+
+  // Y axis controls envelope attack
+  synth.set({
+    envelope: {
+      attack: 0.01 + (y / 100) * 0.99,
+    },
+  });
 }
 
 xyPad.addEventListener("mousedown", function (e) {
